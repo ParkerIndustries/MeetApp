5f9d7c80872a2dbf4a5dfef4f29959fbd4bd0665
ParkerIndustries <mickael.k.swaminathan@gmail.com>
2020-10-13 15:19:11 +0200

    Backend started

--- meet-app/src/App.js
+++ meet-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import MeetCard from './MeetCard';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <MeetCard />
+      <SwipeButtons />
     </div>
   );
 }
